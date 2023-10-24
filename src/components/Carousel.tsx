4e07768c5edaf0f264756cd03ae415488ed6b032
Jack Golden <jackgolden@vt.edu>
2023-10-23 21:20:48 -0400

    moe changes

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './Carousel.css';
+import { useSpring, animated } from 'react-spring';
 import ReactMarkdown from 'react-markdown';
+import linearGIF from '../assets/gif/linearPreview.gif';
+import flowerGIF from '../assets/gif/flowerPreview.gif'
+
+const getImageUrl = (fileName: string) => {
+  const imagePath = Object.keys(images).find(path => path.includes(fileName));
+  return imagePath ? images[imagePath] : null;
+};
 
 interface Project {
   title: string;
@@ -14,26 +22,51 @@ interface Props {
 
 const Carousel: React.FC<Props> = ({ projects }) => {
   const [selectedProject, setSelectedProject] = useState<Project>(projects[0]);
+  const [index, setIndex] = useState(0);
+  const [hovered, setHovered] = useState(false);
+
+  const props = useSpring({
+    opacity: hovered ? 1 : 0,
+    transform: `translate3d(${hovered ? 0 : -50}px,0,0)`,
+  });
 
   useEffect(() => {
-    setSelectedProject(projects[0]);
-  }, [projects]);
+    setSelectedProject(projects[index]);
+  }, [projects, index]);
+
+  const nextProject = () => {
+    setIndex((index + 1) % projects.length);
+  };
+
+  const previousProject = () => {
+    setIndex((index - 1 + projects.length) % projects.length);
+  };
 
   return (
-    <div className="card">
+    <div className="card" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
       <div className="carousel">
+        {projects.map((project, i) => (
+          <img
+            key={i}
+            src={project.image}
+            alt="Project"
+            className={`carousel-image ${i !== index ? 'frozen' : ''}`}
+            style={{ transform: `rotateY(${(i - index) * 30}deg) translateZ(300px)` }}
+          />
+        ))}
+      </div>
+
+      <animated.div className="project-info" style={props}>
         <h2>{selectedProject.title}</h2>
-        <img src={selectedProject.image} alt="Project" className="carousel-image" />
-        <ReactMarkdown className="project-description">
-          {selectedProject.description}
-        </ReactMarkdown>
-        <div className="project-links">
-          {projects.map((project, index) => (
-            <button key={index} onClick={() => setSelectedProject(project)}>
-              {project.title}
-            </button>
-          ))}
-        </div>
+        <ReactMarkdown>{selectedProject.description}</ReactMarkdown> {/* Here's the change */}
+      </animated.div>
+
+      <div className="controls">
+        <button onClick={previousProject}>Previous</button>
+        {projects.map((_, i) => (
+          <span key={i} className={`dot ${i === index ? 'active' : ''}`} onClick={() => setIndex(i)} />
+        ))}
+        <button onClick={nextProject}>Next</button>
       </div>
     </div>
   );
